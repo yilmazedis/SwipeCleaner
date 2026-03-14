e6db8f820f5d5517814e4a4abf5bc9175e78b83c
yilmazedis <ylmazedis@gmail.com>
2026-03-14 22:31:22 +0800

    Initial Commit

---
+++ SwipeCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwipeCleaner
+//
+//  Created by Yılmaz Edis on 14.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwipeCleaner/SwipeCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeCleanerApp.swift
+//  SwipeCleaner
+//
+//  Created by Yılmaz Edis on 14.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwipeCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
